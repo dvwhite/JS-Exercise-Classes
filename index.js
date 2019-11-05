aa859d8ffca30b5e8d1597b39cacaeae172da4f5
David White <dwhite0627@gmail.com>
2019-11-04 19:29:33 -0500

    Add Student class

--- index.js
+++ index.js
@@ -172,8 +172,27 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor (studentArgs) {
+    super(studentArgs);
+    this.previousBackground = studentArgs.previousBackground;
+    this.className = studentArgs.className;
+    this.favSubjects = studentArgs.favSubjects;
+  }
 
+  // Methods
+  listSubjects() {
+    const subjects = this.favSubjects.join(", ");
+    return `Loving ${subjects}!`;
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun a sprint challenge on ${subject}`;
+  }
 }
 
 /*
